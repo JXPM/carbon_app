6a89a60e6a45aa8b8ed58d60bf6b4a2baa1a2f42
JXPM <bilejohan04@gmail.com>
2025-05-13 10:11:16 +0200

    maj

--- Journal.sh
+++ Journal.sh
@@ -8,15 +8,8 @@ gh repo create carbon_app --public
 git remote add origin https://github.com/JXPM/carbon_app.git
 git push --set-upstream origin main
 
-# Supprimer le fichier .env du suivi git
-git rm --cached .env
-rm '.env'
-
-#create .gitignore
-touch backend/.gitignore
-
 #fichier Maj et push
 git status
 git add .
-git commit -m "maj de menu de navigation"
+git commit -m "maj"
 git push origin main
